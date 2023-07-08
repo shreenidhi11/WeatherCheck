d8c114dc99e216a90d2ca50539a4ee4570e02033
shreenidhi11 <shree27061997@gmail.com>
2023-07-08 16:17:51 -0400

    Updated code

--- src/App.js
+++ src/App.js
@@ -3,31 +3,42 @@ import './App.css';
 import Searchbar from './components/searchbar/searchbar';
 import Searchbody from './components/searchbody/searchbody';
 
-const api ={
+const api = {
   base: "https://api.openweathermap.org/data/2.5/",
   key: "6fcebf1698cdaab411a61bb910302974"
 }
 
 function App() {
-    const [place, setplace] = useState('')
-    const takeinputplace = useRef('')
-    const [weather, setweather] = useState({});
+  const [place, setplace] = useState('')
+  const takeinputplace = useRef('')
+  const [weather, setweather] = useState({});
+  // const [error, setError] = useState(false);
 
 
-    const fetchData = async e =>{
-      const currentplace = takeinputplace.current.value
-      const response = await fetch(`${api.base}weather?q=${currentplace}&appid=${api.key}`)
-      const data = await response.json()
-      console.log(data)
-      setplace(takeinputplace.current.value)
-      setweather(data)
+  const fetchData = async e => {
+    const currentplace = takeinputplace.current.value
+    fetch(`${api.base}weather?q=${currentplace}&appid=${api.key}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('API Request Failed')
+        }
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        setplace(takeinputplace.current.value)
+        setweather(data)
+      })
+      .catch(error => {
+        alert('Invalid City Name')
+      })
 
-    }
+  }
 
   return (
     <div className="App">
       <Searchbar onEnter={fetchData} takeinputplace={takeinputplace} />
-      {place && <Searchbody place={place} weather={weather}/>}
+      {place && <Searchbody place={place} weather={weather} />}
     </div>
   );
 }
--- src/components/searchbody/searchbody.js
+++ src/components/searchbody/searchbody.js
@@ -1,31 +1,79 @@
 import React, { useRef, useState } from 'react'
 import './searchbody.css'
+import img from '../searchbody/sun.jpeg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FaWind } from "react-icons/fa6";
+import { AiOutlineCompress } from "react-icons/ai"
+import { WiHumidity } from "react-icons/wi"
 
 function Searchbody(props) {
 
+  function getTodayDay() {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const today = new Date();
+    const dayIndex = today.getDay();
+    return daysOfWeek[dayIndex];
+  }
+
+
+  function getCurrentMonth() {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const today = new Date();
+    const monthIndex = today.getMonth();
+    return months[monthIndex];
+  }
+
+  function getCurrentDate() {
+    const today = new Date();
+    const date = today.getDate();
+    return date;
+  }
+
+  function capitalizeLetter (){
+    const letter = props.weather.weather[0].description
+    return letter.charAt(0).toUpperCase() + letter.slice(1);
+  }
+
+
+
   return (
     <div className='searchbody'>
-
-      <div className='search__place__sky'>
-        <h1>{props.place}</h1>
-        <p>
-          {props.weather.weather[0].description}
-        </p>
-      </div>
-
-      <div className='search__place__details'>
-        <p>
-          Humidity: {props.weather.main.humidity}
-        </p>
-        <p>
-          Pressure : {props.weather.main.pressure}
-        </p>
-        <p>
-          Temperature Range : {props.weather.main.temp_min} - {props.weather.main.temp_max}
-        </p>
+      <div className='Now'>
+        <div className='Now_text'>
+          <h3>Now </h3>
+        </div>
+        <div className='Now_details'>
+          <img className="now_img" src={img} />
+          <h3 className='temp'>35C</h3>
+          <h3 className='temp_desc'>{capitalizeLetter()}</h3>
+        </div>
       </div>
 
 
+      <div className='today'>
+        <div className='today_date'>
+          <h3>Today</h3>
+          <h3>{getTodayDay()}, {getCurrentDate()} {getCurrentMonth()}</h3>
+        </div>
+        <div className='today_details'>
+          <div className='windspeed'>
+            <FaWind  size='1rem'  style={{ color: "#f2f2f2", }} />
+            <h3>WindSpeed {props.weather.wind.speed}mph</h3>
+          </div>
+          <div className='pressure'>
+            <AiOutlineCompress  size='1rem' style={{ color: "#f2f2f2", }} />
+            <h3>
+              Pressure {props.weather.main.pressure}mmHG
+            </h3>
+          </div>
+          <div className='humidity'>
+            <WiHumidity size='1.5rem'  style={{ color: "#f2f2f2", }} />
+            <h3 >
+              Humidity {props.weather.main.humidity}g/m3
+            </h3>
+          </div>
+        </div>
+      </div>
 
     </div>
   )
